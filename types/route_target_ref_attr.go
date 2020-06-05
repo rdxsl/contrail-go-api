d97ef3c59cc01044f7083ac02563ff0ed5c511f7
Maya Raviv <mraviv@riotgames.com>
2020-06-05 10:33:23 -0500

    add missing route_target_refs to routing instance type

---
+++ types/route_target_ref_attr.go
@@ -0,0 +1,5 @@
+package types
+
+type RouteTargetRefAttr struct {
+	ImportExport string `json:"import_export"`
+}
--- types/routing_instance.go
+++ types/routing_instance.go
@@ -5,242 +5,326 @@
 package types
 
 import (
-        "encoding/json"
+	"encoding/json"
 
-        "github.com/Juniper/contrail-go-api"
+	"github.com/Juniper/contrail-go-api"
 )
 
 const (
 	routing_instance_id_perms uint64 = 1 << iota
 	routing_instance_display_name
 	routing_instance_virtual_machine_interface_back_refs
+	routing_instance_virtual_network_back_refs
+	routing_instance_route_target_refs
 )
 
 type RoutingInstance struct {
-        contrail.ObjectBase
-	id_perms IdPermsType
-	display_name string
+	contrail.ObjectBase
+	id_perms                            IdPermsType
+	display_name                        string
 	virtual_machine_interface_back_refs contrail.ReferenceList
-        valid uint64
-        modified uint64
-        baseMap map[string]contrail.ReferenceList
+	virtual_network_back_refs           contrail.ReferenceList
+	route_target_refs                   contrail.ReferenceList
+	valid                               uint64
+	modified                            uint64
+	originalMap                         map[string]contrail.ReferenceList
 }
 
 func (obj *RoutingInstance) GetType() string {
-        return "routing-instance"
+	return "routing-instance"
 }
 
 func (obj *RoutingInstance) GetDefaultParent() []string {
-        name := []string{"default-domain", "default-project", "default-virtual-network"}
-        return name
+	name := []string{"default-domain", "default-project", "default-virtual-network"}
+	return name
 }
 
 func (obj *RoutingInstance) GetDefaultParentType() string {
-        return "virtual-network"
+	return "virtual-network"
 }
 
 func (obj *RoutingInstance) SetName(name string) {
-        obj.VSetName(obj, name)
+	obj.VSetName(obj, name)
 }
 
 func (obj *RoutingInstance) SetParent(parent contrail.IObject) {
-        obj.VSetParent(obj, parent)
+	obj.VSetParent(obj, parent)
 }
 
-func (obj *RoutingInstance) storeReferenceBase(
-        name string, refList contrail.ReferenceList) {
-        if obj.baseMap == nil {
-                obj.baseMap = make(map[string]contrail.ReferenceList)
-        }
-        refCopy := make(contrail.ReferenceList, len(refList))
-        copy(refCopy, refList)
-        obj.baseMap[name] = refCopy
-}
-
-func (obj *RoutingInstance) hasReferenceBase(name string) bool {
-        if obj.baseMap == nil {
-                return false
-        }
-        _, exists := obj.baseMap[name]
-        return exists
+func (obj *RoutingInstance) addChange(
+	name string, refList contrail.ReferenceList) {
+	if obj.originalMap == nil {
+		obj.originalMap = make(map[string]contrail.ReferenceList)
+	}
+	var refCopy contrail.ReferenceList
+	copy(refCopy, refList)
+	obj.originalMap[name] = refCopy
 }
 
 func (obj *RoutingInstance) UpdateDone() {
-        obj.modified = 0
-        obj.baseMap = nil
+	obj.modified = 0
+	obj.originalMap = nil
 }
 
-
 func (obj *RoutingInstance) GetIdPerms() IdPermsType {
-        return obj.id_perms
+	return obj.id_perms
 }
 
 func (obj *RoutingInstance) SetIdPerms(value *IdPermsType) {
-        obj.id_perms = *value
-        obj.modified |= routing_instance_id_perms
+	obj.id_perms = *value
+	obj.modified |= routing_instance_id_perms
 }
 
 func (obj *RoutingInstance) GetDisplayName() string {
-        return obj.display_name
+	return obj.display_name
 }
 
 func (obj *RoutingInstance) SetDisplayName(value string) {
-        obj.display_name = value
-        obj.modified |= routing_instance_display_name
+	obj.display_name = value
+	obj.modified |= routing_instance_display_name
 }
 
 func (obj *RoutingInstance) readVirtualMachineInterfaceBackRefs() error {
-        if !obj.IsTransient() &&
-                (obj.valid & routing_instance_virtual_machine_interface_back_refs == 0) {
-                err := obj.GetField(obj, "virtual_machine_interface_back_refs")
-                if err != nil {
-                        return err
-                }
-        }
-        return nil
+	if !obj.IsTransient() &&
+		(obj.valid&routing_instance_virtual_machine_interface_back_refs == 0) {
+		err := obj.GetField(obj, "virtual_machine_interface_back_refs")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (obj *RoutingInstance) GetVirtualMachineInterfaceBackRefs() (
-        contrail.ReferenceList, error) {
-        err := obj.readVirtualMachineInterfaceBackRefs()
-        if err != nil {
-                return nil, err
-        }
-        return obj.virtual_machine_interface_back_refs, nil
+	contrail.ReferenceList, error) {
+	err := obj.readVirtualMachineInterfaceBackRefs()
+	if err != nil {
+		return nil, err
+	}
+	return obj.virtual_machine_interface_back_refs, nil
+}
+
+func (obj *RoutingInstance) readVirtualNetworkBackRefs() error {
+	if !obj.IsTransient() &&
+		(obj.valid&routing_instance_virtual_network_back_refs == 0) {
+		err := obj.GetField(obj, "virtual_network_back_refs")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (obj *RoutingInstance) GetVirtualNetworkBackRefs() (
+	contrail.ReferenceList, error) {
+	err := obj.readVirtualNetworkBackRefs()
+	if err != nil {
+		return nil, err
+	}
+	return obj.virtual_network_back_refs, nil
+}
+
+func (obj *RoutingInstance) readRouteTargetRefs() error {
+	if !obj.IsTransient() &&
+		(obj.valid&routing_instance_route_target_refs == 0) {
+		err := obj.GetField(obj, "route_target_refs")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (obj *RoutingInstance) GetRouteTargetRefs() (contrail.ReferenceList, error) {
+	err := obj.readRouteTargetRefs()
+	if err != nil {
+		return nil, err
+	}
+	return obj.route_target_refs, nil
+}
+
+func (obj *RoutingInstance) SetRouteTargetRefs(value contrail.ReferenceList) {
+	obj.route_target_refs = value
+	obj.modified |= routing_instance_route_target_refs
 }
 
 func (obj *RoutingInstance) MarshalJSON() ([]byte, error) {
-        msg := map[string]*json.RawMessage {
-        }
-        err := obj.MarshalCommon(msg)
-        if err != nil {
-                return nil, err
-        }
+	msg := map[string]*json.RawMessage{}
+	err := obj.MarshalCommon(msg)
+	if err != nil {
+		return nil, err
+	}
 
-        if obj.modified & routing_instance_id_perms != 0 {
-                var value json.RawMessage
-                value, err := json.Marshal(&obj.id_perms)
-                if err != nil {
-                        return nil, err
-                }
-                msg["id_perms"] = &value
-        }
+	if obj.modified&routing_instance_id_perms != 0 {
+		var value json.RawMessage
+		value, err := json.Marshal(&obj.id_perms)
+		if err != nil {
+			return nil, err
+		}
+		msg["id_perms"] = &value
+	}
 
-        if obj.modified & routing_instance_display_name != 0 {
-                var value json.RawMessage
-                value, err := json.Marshal(&obj.display_name)
-                if err != nil {
-                        return nil, err
-                }
-                msg["display_name"] = &value
-        }
+	if obj.modified&routing_instance_display_name != 0 {
+		var value json.RawMessage
+		value, err := json.Marshal(&obj.display_name)
+		if err != nil {
+			return nil, err
+		}
+		msg["display_name"] = &value
+	}
+	if obj.modified&routing_instance_route_target_refs != 0 {
+		var value json.RawMessage
+		value, err := json.Marshal(&obj.route_target_refs)
+		if err != nil {
+			return nil, err
+		}
+		msg["route_target_refs"] = &value
+	}
 
-        return json.Marshal(msg)
+	return json.Marshal(msg)
 }
 
 func (obj *RoutingInstance) UnmarshalJSON(body []byte) error {
-        var m map[string]json.RawMessage
-        err := json.Unmarshal(body, &m)
-        if err != nil {
-                return err
-        }
-        err = obj.UnmarshalCommon(m)
-        if err != nil {
-                return err
-        }
-        for key, value := range m {
-                switch key {
-                case "id_perms":
-                        err = json.Unmarshal(value, &obj.id_perms)
-                        if err == nil {
-                                obj.valid |= routing_instance_id_perms
-                        }
-                        break
-                case "display_name":
-                        err = json.Unmarshal(value, &obj.display_name)
-                        if err == nil {
-                                obj.valid |= routing_instance_display_name
-                        }
-                        break
-                case "virtual_machine_interface_back_refs": {
-                        type ReferenceElement struct {
-                                To []string
-                                Uuid string
-                                Href string
-                                Attr PolicyBasedForwardingRuleType
-                        }
-                        var array []ReferenceElement
-                        err = json.Unmarshal(value, &array)
-                        if err != nil {
-                            break
-                        }
-                        obj.valid |= routing_instance_virtual_machine_interface_back_refs
-                        obj.virtual_machine_interface_back_refs = make(contrail.ReferenceList, 0)
-                        for _, element := range array {
-                                ref := contrail.Reference {
-                                        element.To,
-                                        element.Uuid,
-                                        element.Href,
-                                        element.Attr,
-                                }
-                                obj.virtual_machine_interface_back_refs = append(obj.virtual_machine_interface_back_refs, ref)
-                        }
-                        break
-                }
-                }
-                if err != nil {
-                        return err
-                }
-        }
-        return nil
+	var m map[string]json.RawMessage
+	err := json.Unmarshal(body, &m)
+	if err != nil {
+		return err
+	}
+	err = obj.UnmarshalCommon(m)
+	if err != nil {
+		return err
+	}
+	for key, value := range m {
+		switch key {
+		case "id_perms":
+			err = json.Unmarshal(value, &obj.id_perms)
+			if err == nil {
+				obj.valid |= routing_instance_id_perms
+			}
+			break
+		case "display_name":
+			err = json.Unmarshal(value, &obj.display_name)
+			if err == nil {
+				obj.valid |= routing_instance_display_name
+			}
+			break
+		case "virtual_network_back_refs":
+			err = json.Unmarshal(value, &obj.virtual_network_back_refs)
+			if err == nil {
+				obj.valid |= routing_instance_virtual_network_back_refs
+			}
+			break
+		case "virtual_machine_interface_back_refs":
+			{
+				type ReferenceElement struct {
+					To   []string
+					Uuid string
+					Href string
+					Attr PolicyBasedForwardingRuleType
+				}
+				var array []ReferenceElement
+				err = json.Unmarshal(value, &array)
+				if err != nil {
+					break
+				}
+				obj.valid |= routing_instance_virtual_machine_interface_back_refs
+				obj.virtual_machine_interface_back_refs = make(contrail.ReferenceList, 0)
+				for _, element := range array {
+					ref := contrail.Reference{
+						element.To,
+						element.Uuid,
+						element.Href,
+						element.Attr,
+					}
+					obj.virtual_machine_interface_back_refs = append(obj.virtual_machine_interface_back_refs, ref)
+				}
+				break
+			}
+		case "route_target_refs":
+			type ReferenceElement struct {
+				To   []string
+				Uuid string
+				Href string
+				Attr RouteTargetRefAttr
+			}
+			var array []ReferenceElement
+			err = json.Unmarshal(value, &array)
+			if err != nil {
+				break
+			}
+			obj.valid |= routing_instance_route_target_refs
+			obj.route_target_refs = make(contrail.ReferenceList, 0)
+			for _, element := range array {
+				ref := contrail.Reference{
+					element.To,
+					element.Uuid,
+					element.Href,
+					element.Attr,
+				}
+				obj.route_target_refs = append(obj.route_target_refs, ref)
+			}
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (obj *RoutingInstance) UpdateObject() ([]byte, error) {
-        msg := map[string]*json.RawMessage {
-        }
-        err := obj.MarshalId(msg)
-        if err != nil {
-                return nil, err
-        }
+	msg := map[string]*json.RawMessage{}
+	err := obj.MarshalId(msg)
+	if err != nil {
+		return nil, err
+	}
 
-        if obj.modified & routing_instance_id_perms != 0 {
-                var value json.RawMessage
-                value, err := json.Marshal(&obj.id_perms)
-                if err != nil {
-                        return nil, err
-                }
-                msg["id_perms"] = &value
-        }
+	if obj.modified&routing_instance_id_perms != 0 {
+		var value json.RawMessage
+		value, err := json.Marshal(&obj.id_perms)
+		if err != nil {
+			return nil, err
+		}
+		msg["id_perms"] = &value
+	}
 
-        if obj.modified & routing_instance_display_name != 0 {
-                var value json.RawMessage
-                value, err := json.Marshal(&obj.display_name)
-                if err != nil {
-                        return nil, err
-                }
-                msg["display_name"] = &value
-        }
-
-        return json.Marshal(msg)
+	if obj.modified&routing_instance_display_name != 0 {
+		var value json.RawMessage
+		value, err := json.Marshal(&obj.display_name)
+		if err != nil {
+			return nil, err
+		}
+		msg["display_name"] = &value
+	}
+	if obj.modified&routing_instance_route_target_refs != 0 {
+		var value json.RawMessage
+		value, err := json.Marshal(&obj.route_target_refs)
+		if err != nil {
+			return nil, err
+		}
+		msg["route_target_refs"] = &value
+	}
+	return json.Marshal(msg)
 }
 
 func (obj *RoutingInstance) UpdateReferences() error {
 
-        return nil
+	return nil
 }
 
 func RoutingInstanceByName(c contrail.ApiClient, fqn string) (*RoutingInstance, error) {
-    obj, err := c.FindByName("routing-instance", fqn)
-    if err != nil {
-        return nil, err
-    }
-    return obj.(*RoutingInstance), nil
+	obj, err := c.FindByName("routing-instance", fqn)
+	if err != nil {
+		return nil, err
+	}
+	return obj.(*RoutingInstance), nil
 }
 
 func RoutingInstanceByUuid(c contrail.ApiClient, uuid string) (*RoutingInstance, error) {
-    obj, err := c.FindByUuid("routing-instance", uuid)
-    if err != nil {
-        return nil, err
-    }
-    return obj.(*RoutingInstance), nil
+	obj, err := c.FindByUuid("routing-instance", uuid)
+	if err != nil {
+		return nil, err
+	}
+	return obj.(*RoutingInstance), nil
 }
